fb7cc5668804c80b6db3d1561d5097b7cb25fa01
Igor <igoribuzian@gmail.com>
2025-05-06 21:01:04 +0300

    a little bit changes in entity clases

--- src/main/java/com/example/spring/entity/Role.java
+++ src/main/java/com/example/spring/entity/Role.java
@@ -2,24 +2,31 @@ package com.example.spring.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Data
 @Table(name="roles")
-public class Role {
+public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
     @Column(nullable = false,unique = true)
     String name;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "role_permissions",
             joinColumns = @JoinColumn(name = "role_id"),
             inverseJoinColumns = @JoinColumn(name = "permission_id")
     )
-    Set<Permission> permissions;
+    Set<Permission> permissions= new HashSet<>();
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
 }
--- src/main/java/com/example/spring/entity/User.java
+++ src/main/java/com/example/spring/entity/User.java
@@ -2,28 +2,68 @@ package com.example.spring.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.validation.constraints.Email;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
-@Entity
+
 @Data
+@Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    int id;
-    @Column(nullable = false,unique = true, length = 100)
-    String username;
-    @Column(nullable = false,length = 100)
-    String password;
-    @Column(nullable = false,unique = true) @Email
-    String email;
-    @ManyToMany
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false, length = 100)
+    private String email;
+
+    private boolean enabled = true;
+
+    @Column(name = "created_at", updatable = false)
+    public LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at")
+    public LocalDateTime updatedAt = LocalDateTime.now();
+
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
-        name = "user_roles",
-           joinColumns =  @JoinColumn(name = "role_id"),
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
-    Set<Role> role;
+    private Set<Role> roles;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
 }
